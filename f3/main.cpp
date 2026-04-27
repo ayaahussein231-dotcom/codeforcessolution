93605bc19de9d0c9c690c0f1d75761f3205e0d62
ayaa hussein <ayaahussein231@gmail.com>
2026-04-27 23:22:21 +0300

    the beginning
    first solution

---
+++ f3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int c;
+   cin>>c;
+   long long g;
+   cin>>g;
+   char n;
+   cin>>n;
+   float f;
+   cin>>f;
+   double d;
+   cin>>d;
+   cout<<c<<endl;
+   cout<<g<<endl;
+   cout<<n<<endl;
+   cout<<f<<endl;
+   cout<<d<<endl;
+
+    return 0;
+}
